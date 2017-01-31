4d3b753029fbc211cb9be9574648df685f3229b9
graemerocher <graeme.rocher@gmail.com>
2017-01-31 15:01:37 +0100

    exclude JTA autoconfiguration

---
+++ skeleton/grails-app/init/@grails.codegen.defaultPackage.path@/Application.groovy
@@ -0,0 +1,13 @@
+package @grails.codegen.defaultPackage@
+
+import org.springframework.boot.autoconfigure.transaction.jta.*
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+@EnableAutoConfiguration(exclude=[JtaAutoConfiguration])
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
