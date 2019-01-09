09fc9abfac403d483ddf783734e2b0a1bbb086f1
sdelamo <sergio.delamo@softamo.com>
2019-01-09 15:20:16 +0100

    Add @CompileStatic to Application

--- skeleton/grails-app/init/@grails.codegen.defaultPackage.path@/Application.groovy
+++ skeleton/grails-app/init/@grails.codegen.defaultPackage.path@/Application.groovy
@@ -1,10 +1,12 @@
 package @grails.codegen.defaultPackage@
 
-import org.springframework.boot.autoconfigure.transaction.jta.*
+import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import groovy.transform.CompileStatic
 
+@CompileStatic
 @EnableAutoConfiguration(exclude=[JtaAutoConfiguration])
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
